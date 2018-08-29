5f8c3412452f3b5ee8783d24c3546df09753a13d
valeria campilongo <cvaleriac@hotmail.it>
2018-08-29 00:37:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,26 +1,24 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    student_hash.each do |attribute, value|
-      self.send("#{attribute}=", value)
-    end
+    student_hash.each {|key, value|
+      self.send("#{key}=", value)}
     @@all << self
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |student_hash|
-      Student.new(student_hash)
-    end
+    students_array.each { |student|
+      Student.new(student)
+    }
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |attr, value|
-      self.send("#{attr}=", value)
-    end
+    attributes_hash.each { |key, value|
+      self.send("#{key}=", value)}
     self
   end
 
