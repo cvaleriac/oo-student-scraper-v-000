ae812219682e4f16532f9ba247bd7fc56f32e89b
valeria campilongo <cvaleriac@hotmail.it>
2018-08-29 00:54:43 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,33 +1,105 @@
+require 'open-uri'
 require 'pry'
+require 'nokogiri' #=> I added this one
+require 'rubygems' #=> I added this one
 class Scraper
+
   def self.scrape_index_page(index_url)
-    students = []
-     student_list = Nokogiri::HTML(open(index_url))
-    student_list.css('div.student-card').each {|student|
-      students << {
-        :name => student.css('h4.student-name').text,
-        :location => student.css('p.student-location').text,
-        :profile_url => "./fixtures/student-site/" + student.css('a').first.attr('href')
-      }
-    }
-    students
+    html = open(index_url) #=> index_url = "../fixtures/student-site/index.html"
+    list = Nokogiri::HTML(html)
+
+    # This block returns a list of student names
+    names = list.css(".student-name")
+    names_array = []
+    names.each do |item|
+      names_array << item.text
+    end
+    names_array
+
+    # This block returns a list of locations.
+    locations = list.css(".student-location")
+    location_array = []
+    locations.each do |item|
+      location_array << item.text
+    end
+    location_array
+
+    # This block returns a list of student HTML pages.
+    webpages = list.css(".student-card a[href]")
+    webpage_array = []
+    webpages.select do |item|
+      webpage_array << item['href']
+    end
+    webpage_array
+
+
+    master_array = []
+    hash = {}
+    x = 0
+    names_array.each do |name|
+      master_array << {:name => name, :location => location_array[x], :profile_url => webpage_array[x]}
+      x = x + 1
+    end
+    master_array
   end
+
   def self.scrape_profile_page(profile_url)
-    profile_data = {}
-     student_page = Nokogiri::HTML(open(profile_url))
-    social_hrefs = student_page.css('div.social-icon-container a').map{
-      |link| link.attribute('href').to_s
-    }
-     twitter = social_hrefs.detect{|i| i.include?('twitter')}
-    profile_data[:twitter] = twitter if twitter
-     linked = social_hrefs.detect{|i| i.include?('linkedin')}
-    profile_data[:linkedin] = linked if linked
-     github = social_hrefs.detect{|i| i.include?('github')}
-    profile_data[:github] = github if github
-     blog = social_hrefs.detect{|i| /(\.com\/\z)|(\.com\z)/ === i}
-    profile_data[:blog] = blog if blog
-     profile_data[:profile_quote] = student_page.css('div.profile-quote').text
-     profile_data[:bio] = student_page.css('div.bio-content.content-holder div.description-holder p').text
-     profile_data
-   end
- end
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+
+    students_twitter = []
+    students_linkedin = []
+    students_github = []
+    students_youtube = []
+    students_blog = []
+
+    doc.xpath('//div[@class="social-icon-container"]/a').map do |link|
+      if link['href'].include?("twitter")
+        students_twitter << link['href']
+      end
+    end
+
+    doc.xpath('//div[@class="social-icon-container"]/a').map do |link|
+      if link['href'].include?("linkedin")
+        students_linkedin << link['href']
+      end
+    end
+
+    doc.xpath('//div[@class="social-icon-container"]/a').map do |link|
+      if link['href'].include?("github")
+        students_github << link['href']
+      end
+    end
+
+    doc.xpath('//div[@class="social-icon-container"]/a').map do |link|
+      if !(link['href'].include?("github")) &&
+         !(link['href'].include?("linkedin")) &&
+         !(link['href'].include?("twitter"))
+         students_blog = link['href']
+      end
+    end
+
+    students_quote = doc.css(".profile-quote").text
+    students_bio = doc.css(".description-holder p").text
+
+    students_twitter
+    students_linkedin
+    students_github
+    students_youtube
+      if students_youtube = ""
+        students_youtube = nil
+      end
+    students_blog
+
+    # This needs to be an iteration where it starts with <<output = {}>>
+    out_put = {:twitter=>students_twitter[0],
+               :linkedin=>students_linkedin[0],
+               :github=>students_github[0],
+               :profile_quote=>students_quote,
+               :blog=>students_blog,
+               :bio=> students_bio}
+    out_put.delete_if {|key, value| value == [] || value == nil}
+
+    out_put
+  end
+end
